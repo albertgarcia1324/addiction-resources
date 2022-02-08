527703677a6d1325a190a076fe64bc7b7cb5f9ca
Albert Garcia <albert.garcia1324@gmail.com>
2022-02-08 12:26:10 -0500

    colors

--- index.js
+++ index.js
@@ -8,24 +8,17 @@ $(function () {
     $('#urgentModal').modal(options);
 })
 
-// TOOLTIPS
-
-// $(document).ready(function(){
-//     $('#immediateHelp').tooltip('hide')
-// });
-
-// $(document).ready(function(){
-//     $('#immediateHelp').tooltip('toggle')
-// });
 
 $(".immediate-help").hover (
     function() {
         const $this = $(this);
         $this.data('defaultText', $this.text());
         $this.text("Well-Mobile 410-222-7095 and Crisis 410-768-5522");
+        $this.css('color', 'red');
     },
     function() {
         const $this = $(this);
         $this.text($this.data('defaultText'));
+        $this.css('color', 'black');
     }
 );
